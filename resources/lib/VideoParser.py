3f67161dc623a8ab11c6842ac07eaf68853cfbd4
James A. Tucker <jtucker1972@gmail.com>
2011-05-05 14:38:17 -0400

    finished adding updates from PseudoTV update ddd13813

--- resources/lib/VideoParser.py
+++ resources/lib/VideoParser.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with PseudoTV.  If not, see <http://www.gnu.org/licenses/>.
 
-import xbmc
+import xbmc, xbmcvfs
 import os, platform
 import subprocess
 
